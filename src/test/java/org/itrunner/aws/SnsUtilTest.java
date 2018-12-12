d84e161c81d9315d2ff00d1bffa3ebf2fb41156c
sunjc@iata.org <sunjc@iata.org>
2018-12-12 17:15:32 +0800

    Add files via upload

--- src/test/java/org/itrunner/aws/SnsUtilTest.java
+++ src/test/java/org/itrunner/aws/SnsUtilTest.java
@@ -8,7 +8,7 @@ import static org.itrunner.aws.sns.SnsUtil.*;
 public class SnsUtilTest {
     public static void main(String[] args) {
         CreateTopicResult topic = createTopic("test-topic");
-        SubscribeResult subscribe = subscribe(topic.getTopicArn(), "email", "sunjc@iata.org");
+        SubscribeResult subscribe = subscribe(topic.getTopicArn(), "email", "jason@163.com");
 //        confirmSubscription("arn:aws-cn:sns:cn-north-1:891245299999:test-topic", "...");
 //        publish("arn:aws-cn:sns:cn-north-1:891245299999:test-topic", "Hello COCO", "Hello COCO");
 //        unsubscribe("arn:aws-cn:sns:cn-north-1:891245299999:test-topic:bcd65f82-ae54-4604-a763-30b7ff877e8a");
